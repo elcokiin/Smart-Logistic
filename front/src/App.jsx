e591756f9e75d9e3b0c290e206db2a9965773145
Diego Tenjo <tenjodiego961@gmail.com>
2025-03-29 22:22:21 -0500

    Init commit with initial project structure

---
+++ front/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App;
