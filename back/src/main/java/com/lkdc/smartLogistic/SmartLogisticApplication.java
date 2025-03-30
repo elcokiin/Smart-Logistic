a9ee9ab5d8d522bb5f0f01c52ae63ba56fa23549
Diego Tenjo <tenjodiego961@gmail.com>
2025-03-29 22:23:33 -0500

    Init commit with initial project structure for back

---
+++ back/src/main/java/com/lkdc/smartLogistic/SmartLogisticApplication.java
@@ -0,0 +1,13 @@
+package com.lkdc.smartLogistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLogisticApplication.class, args);
+	}
+
+}
